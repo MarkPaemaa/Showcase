26324574fa55b85c582d617418ff7d20b3af0c84
Mark Paemaa <mark@paemaa.com>
2015-04-30 15:23:54 +1000

    Updates to Technology Pages and various other small additions

---
+++ MarkPaemaaShowCase/MarkPaemaaShowCase/Areas/Technologies/Controllers/CSharpController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MarkPaemaaShowCase.Areas.Technologies.Controllers
+{
+    public class CSharpController : Controller
+    {
+        // GET: Technologies/CSharp
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MarkPaemaaShowCase/MarkPaemaaShowCase/Areas/Technologies/Controllers/InterfacesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MarkPaemaaShowCase.Areas.Technologies.Controllers
+{
+    public class InterfacesController : Controller
+    {
+        // GET: Technologies/Interfaces
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
